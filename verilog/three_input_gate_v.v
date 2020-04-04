06bfe4b68546b468ef3c3c12a6daa4503dba2f31
Brandon Valley <bavvh8@mst.edu>
2020-04-04 17:10:43 -0500

    VVV b model works

--- verilog/three_input_gate_v.v
+++ verilog/three_input_gate_v.v
@@ -5,9 +5,7 @@
 ////////////////////////////////
 
 module three_input_gate_v__behavior
-  // (input  [3:0] i_code,
-   // output [1:0] o_code,
-   // output       o_valid);
+
   (
   input        a     ,
   input        b     ,
@@ -17,7 +15,19 @@ module three_input_gate_v__behavior
   );
   
   
-  assign o_f = 1;
+  assign o_f = i_code == 00 ?    a ^ b ^ c    : 
+               i_code == 01 ? ~ (a & b & c)   : 
+               i_code == 10 ? ~ ( a |  b | c) : 
+                              (~a & ~b & ~c) | (~a & b & c) | (a & ~b & c) | (a & b & ~c);
+  
+  
+  
+  
+  
+  
+  
+  
+  // assign o_f = 1;
   
    
   // // behavior model
@@ -38,56 +48,4 @@ module three_input_gate_v__behavior
            
                  
 endmodule
-
-
-// ////////////////////////////////
-// // Using Always
-// ////////////////////////////////
-// module three_input_gate_v__always (
-  // input  [3:0] i_code,
-  // output reg [1:0] o_code,
-  // output reg     o_valid);
-
-  // always @*
-    // case (i_code)
-      // 4'b0000 : begin o_code = 2'b00; o_valid = 0; end
-      // 4'b0001 : begin o_code = 2'b00; o_valid = 1; end
-      // 4'b0010 : begin o_code = 2'b01; o_valid = 1; end
-      // 4'b0011 : begin o_code = 2'b00; o_valid = 1; end
-      // 4'b0100 : begin o_code = 2'b10; o_valid = 1; end
-      // 4'b0101 : begin o_code = 2'b00; o_valid = 1; end
-      // 4'b0110 : begin o_code = 2'b01; o_valid = 1; end
-      // 4'b0111 : begin o_code = 2'b00; o_valid = 1; end
-      // 4'b1000 : begin o_code = 2'b11; o_valid = 1; end
-      // 4'b1001 : begin o_code = 2'b00; o_valid = 1; end
-      // 4'b1010 : begin o_code = 2'b01; o_valid = 1; end
-      // 4'b1011 : begin o_code = 2'b00; o_valid = 1; end
-      // 4'b1100 : begin o_code = 2'b10; o_valid = 1; end
-      // 4'b1101 : begin o_code = 2'b00; o_valid = 1; end
-      // 4'b1110 : begin o_code = 2'b01; o_valid = 1; end
-      // 4'b1111 : begin o_code = 2'b00; o_valid = 1; end
-    // endcase
-                 
-// endmodule
-
-
-// ////////////////////////////////
-// //  Equation Model
-// ////////////////////////////////
-// module three_input_gate_v__equation
-  // (input  [3:0] i_code,
-   // output [1:0] o_code,
-   // output       o_valid);
-  
-  
-  // assign o_code[0] = (i_code[1] & ~i_code[0]) | (i_code[3] & ~i_code[2] & ~i_code[0]);
-  // assign o_code[1] = (i_code[2] & ~i_code[1] & ~i_code[0]) | (i_code[3] & ~i_code[1] & ~i_code[0]);
- 
-  // // equation model
-  // assign o_valid  = i_code[0] |
-                    // i_code[1] |
-                    // i_code[2] |
-                    // i_code[3];
-                 
-// endmodule
 
