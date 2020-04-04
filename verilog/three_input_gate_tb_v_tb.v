7903bf62d72a977a9ea2319fa709b8fec9b457c8
Brandon Valley <bavvh8@mst.edu>
2020-04-04 16:42:56 -0500

    VVV test sim works

--- verilog/three_input_gate_tb_v_tb.v
+++ verilog/three_input_gate_tb_v_tb.v
@@ -4,26 +4,54 @@
 
 module three_input_gate_tb_v;
   
-  reg  [3:0] i_code;
-  wire [1:0] o_code;
-  wire       o_valid;  
+  // reg  [3:0] i_code;
+  // wire [1:0] o_code;
+  // wire       o_valid;  
+  
+  reg        a     ;
+  reg        b     ;
+  reg        c     ;
+  reg  [1:0] i_code;
+  wire       o_f   ;
   
   // reg i_a, i_b, i_c, i_d;
-  reg [3:0] d_in = 4'b0000;
+  reg [4:0] d_in = 5'b00000;
   integer i;
   
   // duv port map options:
   // three_input_gate_v__no_always duv (.i_code(i_code), .o_code(o_code), .o_valid(o_valid));  // BEHAVIOR MODEL!!!!!!!!!!!!!!!!!!
   // three_input_gate_v__always duv (.i_code(i_code), .o_code(o_code), .o_valid(o_valid)); 
-  three_input_gate_v__equation duv (.i_code(i_code), .o_code(o_code), .o_valid(o_valid)); 
+  // three_input_gate_v__equation duv (.i_code(i_code), .o_code(o_code), .o_valid(o_valid)); 
+  // three_input_gate_v__equation duv (
+  three_input_gate_v__behavior duv (
+                                      .a     (a     ),
+                                      .b     (b     ),
+                                      .c     (c     ),
+                                      .i_code(i_code),
+                                      .o_f   (o_f   )
+                                    );
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    // .i_code(i_code), .o_code(o_code), .o_valid(o_valid)); 
   
   //procedure statement
   initial begin
 
       
       
-      for (i = 0 ; i < 18 ; i = i + 1) begin
-        #1000 i_code = d_in;
+      for (i = 0 ; i < 34 ; i = i + 1) begin
+        #1000 a      = d_in[0];
+              b      = d_in[1];
+              c      = d_in[2];
+              i_code = d_in[4:3];
+      
+      
+      
+        // #1000 i_code = d_in;
         // #1000 o_code[0] = d_in[0]; o_code[1] = d_in[1]; o_code[2] = d_in[]; i_d = d_in[4];
         d_in = d_in + 1;
       end
--- verilog/three_input_gate_v.v
+++ verilog/three_input_gate_v.v
@@ -1,80 +1,93 @@
 // -- python C:\Users\Brandon\Documents\Personal_Projects\my_utils\modelsim_utils\auto_run.py -d run_cmd__three_input_gate_v.do
-//
+
 ////////////////////////////////
-// Not Using Always - Behavior
+//  Behavior Model
 ////////////////////////////////
 
-module three_input_gate_v__no_always
-  (input  [3:0] i_code,
-   output [1:0] o_code,
-   output       o_valid);
+module three_input_gate_v__behavior
+  // (input  [3:0] i_code,
+   // output [1:0] o_code,
+   // output       o_valid);
+  (
+  input        a     ,
+  input        b     ,
+  input        c     ,
+  input  [1:0] i_code,
+  output       o_f
+  );
+  
+  
+  assign o_f = 1;
+  
    
-  // behavior model
-  // line 0 hase highest priority
-  assign o_code = i_code[0] ? 2'b00:
-                  i_code[1] ? 2'b01:
-                  i_code[2] ? 2'b10:
-                  i_code[3] ? 2'b11:
-                  2'b00;
+  // // behavior model
+  // // line 0 hase highest priority
+  // assign o_code = i_code[0] ? 2'b00:
+                  // i_code[1] ? 2'b01:
+                  // i_code[2] ? 2'b10:
+                  // i_code[3] ? 2'b11:
+                  // 2'b00;
            
-  assign o_valid = i_code[0] ? 1'b1:
-                   i_code[1] ? 1'b1:
-                   i_code[2] ? 1'b1:
-                   i_code[3] ? 1'b1:
-                   1'b0;
+  // assign o_valid = i_code[0] ? 1'b1:
+                   // i_code[1] ? 1'b1:
+                   // i_code[2] ? 1'b1:
+                   // i_code[3] ? 1'b1:
+                   // 1'b0;
+                   
+                   
            
                  
 endmodule
 
 
-////////////////////////////////
-// Using Always
-////////////////////////////////
-module three_input_gate_v__always (
-  input  [3:0] i_code,
-  output reg [1:0] o_code,
-  output reg     o_valid);
+// ////////////////////////////////
+// // Using Always
+// ////////////////////////////////
+// module three_input_gate_v__always (
+  // input  [3:0] i_code,
+  // output reg [1:0] o_code,
+  // output reg     o_valid);
 
-  always @*
-    case (i_code)
-      4'b0000 : begin o_code = 2'b00; o_valid = 0; end
-      4'b0001 : begin o_code = 2'b00; o_valid = 1; end
-      4'b0010 : begin o_code = 2'b01; o_valid = 1; end
-      4'b0011 : begin o_code = 2'b00; o_valid = 1; end
-      4'b0100 : begin o_code = 2'b10; o_valid = 1; end
-      4'b0101 : begin o_code = 2'b00; o_valid = 1; end
-      4'b0110 : begin o_code = 2'b01; o_valid = 1; end
-      4'b0111 : begin o_code = 2'b00; o_valid = 1; end
-      4'b1000 : begin o_code = 2'b11; o_valid = 1; end
-      4'b1001 : begin o_code = 2'b00; o_valid = 1; end
-      4'b1010 : begin o_code = 2'b01; o_valid = 1; end
-      4'b1011 : begin o_code = 2'b00; o_valid = 1; end
-      4'b1100 : begin o_code = 2'b10; o_valid = 1; end
-      4'b1101 : begin o_code = 2'b00; o_valid = 1; end
-      4'b1110 : begin o_code = 2'b01; o_valid = 1; end
-      4'b1111 : begin o_code = 2'b00; o_valid = 1; end
-    endcase
+  // always @*
+    // case (i_code)
+      // 4'b0000 : begin o_code = 2'b00; o_valid = 0; end
+      // 4'b0001 : begin o_code = 2'b00; o_valid = 1; end
+      // 4'b0010 : begin o_code = 2'b01; o_valid = 1; end
+      // 4'b0011 : begin o_code = 2'b00; o_valid = 1; end
+      // 4'b0100 : begin o_code = 2'b10; o_valid = 1; end
+      // 4'b0101 : begin o_code = 2'b00; o_valid = 1; end
+      // 4'b0110 : begin o_code = 2'b01; o_valid = 1; end
+      // 4'b0111 : begin o_code = 2'b00; o_valid = 1; end
+      // 4'b1000 : begin o_code = 2'b11; o_valid = 1; end
+      // 4'b1001 : begin o_code = 2'b00; o_valid = 1; end
+      // 4'b1010 : begin o_code = 2'b01; o_valid = 1; end
+      // 4'b1011 : begin o_code = 2'b00; o_valid = 1; end
+      // 4'b1100 : begin o_code = 2'b10; o_valid = 1; end
+      // 4'b1101 : begin o_code = 2'b00; o_valid = 1; end
+      // 4'b1110 : begin o_code = 2'b01; o_valid = 1; end
+      // 4'b1111 : begin o_code = 2'b00; o_valid = 1; end
+    // endcase
                  
-endmodule
+// endmodule
 
 
-////////////////////////////////
-//  Equation Model
-////////////////////////////////
-module three_input_gate_v__equation
-  (input  [3:0] i_code,
-   output [1:0] o_code,
-   output       o_valid);
+// ////////////////////////////////
+// //  Equation Model
+// ////////////////////////////////
+// module three_input_gate_v__equation
+  // (input  [3:0] i_code,
+   // output [1:0] o_code,
+   // output       o_valid);
   
   
-  assign o_code[0] = (i_code[1] & ~i_code[0]) | (i_code[3] & ~i_code[2] & ~i_code[0]);
-  assign o_code[1] = (i_code[2] & ~i_code[1] & ~i_code[0]) | (i_code[3] & ~i_code[1] & ~i_code[0]);
+  // assign o_code[0] = (i_code[1] & ~i_code[0]) | (i_code[3] & ~i_code[2] & ~i_code[0]);
+  // assign o_code[1] = (i_code[2] & ~i_code[1] & ~i_code[0]) | (i_code[3] & ~i_code[1] & ~i_code[0]);
  
-  // equation model
-  assign o_valid  = i_code[0] |
-                    i_code[1] |
-                    i_code[2] |
-                    i_code[3];
+  // // equation model
+  // assign o_valid  = i_code[0] |
+                    // i_code[1] |
+                    // i_code[2] |
+                    // i_code[3];
                  
-endmodule
+// endmodule
 
