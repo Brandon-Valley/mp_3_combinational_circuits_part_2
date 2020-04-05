18803536ef8a3449d4347522e08a8b0dd9e9acf7
Brandon Valley <bavvh8@mst.edu>
2020-04-05 15:36:51 -0500

    unsigned calc v init sim works

---
+++ verilog/unsigned_calc_tb_v.v
@@ -0,0 +1,45 @@
+// -- python C:\Users\Brandon\Documents\Personal_Projects\my_utils\modelsim_utils\auto_run.py -d run_cmd__unsigned_calc_v.do
+
+`timescale 1ms/1ms
+
+module unsigned_calc_tb_v;
+
+
+  reg  i_a    ;
+  reg  i_b    ;
+  reg  i_carry;
+  wire o_s    ;
+  wire o_carry;
+  
+  reg [4:0] d_in = 5'b00000;
+  integer i;
+  
+  // duv port map options:
+  unsigned_calc_v  duv (
+                            .i_a    (i_a    ),
+                            .i_b    (i_b    ),
+                            .i_carry(i_carry),
+                            .o_s    (o_s    ),
+                            .o_carry(o_carry)
+  );
+
+  //procedure statement
+  initial begin
+
+      //                      |
+      //                      |
+      //                      V
+      for (i = 0 ; i < ( 2 ** 3 ) + 2 ; i = i + 1) begin
+        #1000 i_a     = d_in[0];
+              i_b     = d_in[1];
+              i_carry = d_in[2];
+              
+      
+        // #1000 i_code = d_in;
+        // #1000 o_code[0] = d_in[0]; o_code[1] = d_in[1]; o_code[2] = d_in[]; i_d = d_in[4];
+        d_in = d_in + 1;
+      end
+          
+    end
+
+endmodule
---
+++ verilog/unsigned_calc_v.v
@@ -0,0 +1,74 @@
+// -- python C:\Users\Brandon\Documents\Personal_Projects\my_utils\modelsim_utils\auto_run.py -d run_cmd__unsigned_calc_v.do
+
+////////////////////////////
+ // Equation Model
+////////////////////////////
+module unsigned_calc_v
+  (
+     input  i_a     ,
+     input  i_b     ,
+     input  i_carry ,
+     output o_s     ,
+     output o_carry
+   );
+ 
+  assign o_s     = i_a ^ i_b ^ i_carry;
+  assign o_carry = ((i_a ^ i_b) & i_carry) | (i_a & i_b);
+    
+endmodule 
+
+
+
+
+// ////////////////////////////////
+// //  Behavior Model
+// ////////////////////////////////
+// module unsigned_calc_v
+  // (
+     // input  i_a     ,
+     // input  i_b     ,
+     // input  i_carry ,
+     // output o_s     ,
+     // output o_carry
+   // );
+ 
+  // assign o_s     = 0  ?  ! i_a && ! i_b && ! i_carry  :
+                   // 1  ?  ! i_a && ! i_b &&   i_carry  :
+                   // 1  ?  ! i_a &&   i_b && ! i_carry  :
+                   // 0  ?  ! i_a &&   i_b &&   i_carry  :
+                   // 1  ?    i_a && ! i_b && ! i_carry  :
+                   // 0  ?    i_a && ! i_b &&   i_carry  :
+                   // 0  ?    i_a &&   i_b && ! i_carry  :
+                   // 1  ?    i_a &&   i_b &&   i_carry  : 0; // never else
+                                                
+  // assign o_carry = 0  ?  ! i_a && ! i_b && ! i_carry  :
+                   // 0  ?  ! i_a && ! i_b &&   i_carry  :
+                   // 0  ?  ! i_a &&   i_b && ! i_carry  :
+                   // 1  ?  ! i_a &&   i_b &&   i_carry  :
+                   // 0  ?    i_a && ! i_b && ! i_carry  :
+                   // 1  ?    i_a && ! i_b &&   i_carry  :
+                   // 1  ?    i_a &&   i_b && ! i_carry  :
+                   // 1  ?    i_a &&   i_b &&   i_carry  : 0; // never else                       
+// endmodule 
+
+
+
+// //////////////////////////////////////////
+// //  Component Model - With Half Adders
+// //////////////////////////////////////////
+// module unsigned_calc_v
+  // (
+     // input  i_a     ,
+     // input  i_b     ,
+     // input  i_carry ,
+     // output o_s     ,
+     // output o_carry
+   // );
+  // wire ha_1_co, ha_1_s, ha_2_co;
+  // half_adder_v ha1 (i_a     , i_b    , ha_1_co, ha_1_s);
+  // half_adder_v ha2 (ha_1_s  , i_carry, ha_2_co, o_s );
+  // assign o_carry = ((i_a ^ i_b) & i_carry) | (i_a & i_b);
+// endmodule 
+
+
+
