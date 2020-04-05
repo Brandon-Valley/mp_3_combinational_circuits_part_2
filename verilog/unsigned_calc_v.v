4b4a0546ca9a5a7a78f1bd860b09b31ad94d3feb
Brandon Valley <bavvh8@mst.edu>
2020-04-05 16:15:25 -0500

    in middle, VVV cmpnt test sim working

--- verilog/unsigned_calc_v.v
+++ verilog/unsigned_calc_v.v
@@ -1,8 +1,25 @@
 // -- python C:\Users\Brandon\Documents\Personal_Projects\my_utils\modelsim_utils\auto_run.py -d run_cmd__unsigned_calc_v.do
 
-////////////////////////////
-//  Behavior Model
-////////////////////////////
+// ////////////////////////
+ // // Behavior Model
+// ////////////////////////
+// module unsigned_calc_v
+  // (
+     // input  unsigned [3:0] i_au ,
+     // input  unsigned [3:0] i_bu ,
+     // input  unsigned [3:0] i_cu ,
+     // output unsigned [7:0] o_fu 
+   // );
+  
+  // assign o_fu = (7 * i_au) - (3 * i_bu) + (6 * i_cu);
+    
+// endmodule 
+
+
+// 7X-3Y+6Z
+////////////////////////
+//  Component Model
+////////////////////////
 module unsigned_calc_v
   (
      input  unsigned [3:0] i_au ,
@@ -10,49 +27,33 @@ module unsigned_calc_v
      input  unsigned [3:0] i_cu ,
      output unsigned [7:0] o_fu 
    );
- 
-  // assign o_s     = i_a ^ i_b ^ i_carry;
-  // assign o_carry = ((i_a ^ i_b) & i_carry) | (i_a & i_b);
-  
-  // assign o_fu = 00010010;
   
   assign o_fu = (7 * i_au) - (3 * i_bu) + (6 * i_cu);
+  
+  // internal 
+  // reg unsigned [6:0] au_i_m = 7 * i_au;
+  // reg unsigned [6:0] bu_i_m = 7 * i_au;
+  // reg unsigned [6:0] cu_i_m = 3 * i_bu;
+  
+  wire unsigned [6:0] au_i_m;
+  wire unsigned [6:0] bu_i_m;
+  wire unsigned [6:0] cu_i_m;
+  
+  reg unsigned [6:0] sum_ab;
+  reg unsigned [6:0] sum_abc;
+  
+  wire fa_0_co;
+  wire fa_1_co;
+  
+  assign au_i_m = 7 * i_au;
+  assign bu_i_m = 3 * i_bu;
+  assign cu_i_m = 6 * i_cu;
+  
+  assign o_fu = (7 * i_au) - (3 * i_bu) + (6 * i_cu);
+
     
 endmodule 
 
-
-
-
-// ////////////////////////////////
-// //  Behavior Model
-// ////////////////////////////////
-// module unsigned_calc_v
-  // (
-     // input  i_a     ,
-     // input  i_b     ,
-     // input  i_carry ,
-     // output o_s     ,
-     // output o_carry
-   // );
- 
-  // assign o_s     = 0  ?  ! i_a && ! i_b && ! i_carry  :
-                   // 1  ?  ! i_a && ! i_b &&   i_carry  :
-                   // 1  ?  ! i_a &&   i_b && ! i_carry  :
-                   // 0  ?  ! i_a &&   i_b &&   i_carry  :
-                   // 1  ?    i_a && ! i_b && ! i_carry  :
-                   // 0  ?    i_a && ! i_b &&   i_carry  :
-                   // 0  ?    i_a &&   i_b && ! i_carry  :
-                   // 1  ?    i_a &&   i_b &&   i_carry  : 0; // never else
-                                                
-  // assign o_carry = 0  ?  ! i_a && ! i_b && ! i_carry  :
-                   // 0  ?  ! i_a && ! i_b &&   i_carry  :
-                   // 0  ?  ! i_a &&   i_b && ! i_carry  :
-                   // 1  ?  ! i_a &&   i_b &&   i_carry  :
-                   // 0  ?    i_a && ! i_b && ! i_carry  :
-                   // 1  ?    i_a && ! i_b &&   i_carry  :
-                   // 1  ?    i_a &&   i_b && ! i_carry  :
-                   // 1  ?    i_a &&   i_b &&   i_carry  : 0; // never else                       
-// endmodule 
 
 
 
