7696f80bb55659938bd9d961a707fc7d684eb159
Brandon Valley <bavvh8@mst.edu>
2020-04-05 15:15:18 -0500

    works, about to go back to get unsigned_calc.h

--- verilog/full_adder_v.v
+++ verilog/full_adder_v.v
@@ -1,21 +1,21 @@
 // -- python C:\Users\Brandon\Documents\Personal_Projects\my_utils\modelsim_utils\auto_run.py -d run_cmd__full_adder_v.do
 
-// ////////////////////////////
- // // Equation Model
-// ////////////////////////////
-// module full_adder_v
-  // (
-     // input  i_a     ,
-     // input  i_b     ,
-     // input  i_carry ,
-     // output o_s     ,
-     // output o_carry
-   // );
+////////////////////////////
+ // Equation Model
+////////////////////////////
+module full_adder_v
+  (
+     input  i_a     ,
+     input  i_b     ,
+     input  i_carry ,
+     output o_s     ,
+     output o_carry
+   );
  
-  // assign o_s     = i_a ^ i_b ^ i_carry;
-  // assign o_carry = ((i_a ^ i_b) & i_carry) | (i_a & i_b);
+  assign o_s     = i_a ^ i_b ^ i_carry;
+  assign o_carry = ((i_a ^ i_b) & i_carry) | (i_a & i_b);
     
-// endmodule 
+endmodule 
 
 
 
@@ -53,22 +53,22 @@
 
 
 
-//////////////////////////////////////////
-//  Component Model - With Half Adders
-//////////////////////////////////////////
-module full_adder_v
-  (
-     input  i_a     ,
-     input  i_b     ,
-     input  i_carry ,
-     output o_s     ,
-     output o_carry
-   );
-  wire ha_1_co, ha_1_s, ha_2_co;
-  half_adder_v ha1 (i_a     , i_b    , ha_1_co, ha_1_s);
-  half_adder_v ha2 (ha_1_s  , i_carry, ha_2_co, o_s );
-  assign o_carry = ((i_a ^ i_b) & i_carry) | (i_a & i_b);
-endmodule 
+// //////////////////////////////////////////
+// //  Component Model - With Half Adders
+// //////////////////////////////////////////
+// module full_adder_v
+  // (
+     // input  i_a     ,
+     // input  i_b     ,
+     // input  i_carry ,
+     // output o_s     ,
+     // output o_carry
+   // );
+  // wire ha_1_co, ha_1_s, ha_2_co;
+  // half_adder_v ha1 (i_a     , i_b    , ha_1_co, ha_1_s);
+  // half_adder_v ha2 (ha_1_s  , i_carry, ha_2_co, o_s );
+  // assign o_carry = ((i_a ^ i_b) & i_carry) | (i_a & i_b);
+// endmodule 
 
 
 
